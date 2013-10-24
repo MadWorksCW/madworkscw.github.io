f0139a214a7c65b3f8cfca523c3117bc81d07987
Mark Norman <markybob22@gmail.com>
2013-10-24 12:59:44 -0500

    Updates to CSS structure, mobile layout for responsive design

--- js/core.js
+++ js/core.js
@@ -3,7 +3,9 @@ $(function () {
 	$('.logo').animate({ top: 20 }, 1000, 'easeOutBounce');
 	$('.footer').animate({ bottom: 0 }, 1000, 'easeOutBounce', function () {
 		setTimeout(function () {
-			$('.coming-soon').fadeIn(1000);
+			$('.coming-soon').fadeIn(1000, function () {
+				$(this).css('display', 'block');
+			});
 			setTimeout(function () {
 				$('.twitter-timeline-wrapper').animate({ bottom: 150 }, 1000, 'easeOutQuint');
 			}, 500);
